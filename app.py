feab368550d92fd93ee13ac05683b2309faf6192
Niranjan Kashyap K S <122091993+voltsunorsis@users.noreply.github.com>
2025-05-04 17:34:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -235,4 +235,8 @@ def generate_report():
 # ++++++++++++++++++++++++++++++++++++++ END OF REPORT PAGE +++++++++++++++++++++++++++++
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    init_db()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
